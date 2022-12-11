43fba36531d62cdb544c61bfe3626cc3dc9ef93e
preska <wolfwar@outlook.com>
2022-12-11 20:36:24 +0100

    init and chat list item

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ChatListItem from './src/components/ChatListItem';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+
+export default function ChatListItem() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text style={styles.name} numberOfLines={1}>
+            Lukas
+          </Text>
+          <Text style={styles.subTitle}>8:30</Text>
+        </View>
+        <Text style={styles.subTitle} numberOfLines={2}>
+          trolo
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'lightgray',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: 'gray',
+  },
+});
