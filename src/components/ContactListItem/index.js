fb88d2a52ebbbe5b4782bd42b4ea43f27cdd5d85
preska <wolfwar@outlook.com>
2022-12-16 21:09:46 +0100

    backend data model - get users

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -40,6 +40,9 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginRight: 10,
   },
+  content: {
+    flex: 1,
+  },
   name: {
     fontWeight: 'bold',
   },
--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,12 +1,23 @@
+import { API, graphqlOperation } from 'aws-amplify';
+import { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
-import chats from '../../assets/data/chats.json';
 import ContactListItem from '../components/ContactListItem';
+import { listUsers } from '../graphql/queries';
 
 const ContactsScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    API.graphql(graphqlOperation(listUsers)).then((result) => {
+      setUsers(result?.data?.listUsers?.items);
+    });
+  }, []);
+
+  listUsers;
   return (
     <FlatList
-      data={chats || []}
-      renderItem={({ item }) => <ContactListItem user={item.user} />}
+      data={users}
+      renderItem={({ item }) => <ContactListItem user={item} />}
       style={{ backgroundColor: 'white' }}
     />
   );
