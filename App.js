70576c27a4cec65462bdfe64fc557aca8c95fb9f
preska <wolfwar@outlook.com>
2022-12-16 22:38:40 +0100

    add user to DB if doesn't exists

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Navigator from './src/Navigation';
-import { Amplify } from 'aws-amplify';
+import { Amplify, API, Auth, graphqlOperation } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
+import { useEffect } from 'react';
+import { createUser } from './src/graphql/mutations';
+import { getUser } from './src/graphql/queries';
 
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 const App = () => {
+  useEffect(() => {
+    const syncUser = async () => {
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes?.sub })
+      );
+
+      if (userData?.data?.getUser) {
+        console.log('user exists in DB');
+        return;
+      }
+      console.log('stvaram novog usera');
+      const newUser = {
+        id: authUser?.attributes?.sub,
+        name: authUser?.attributes?.phone_number,
+        status: "Hey, I'm using wazzup",
+      };
+
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, {
+          input: newUser,
+        })
+      );
+
+      console.log(newUserResponse);
+    };
+
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Navigator />
